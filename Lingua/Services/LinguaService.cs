b12c3c3518a8b41d4e224f2287d2104e1de816a6
danilovcpp <danilov.cpp@gmail.com>
2018-06-12 08:57:06 +0600

    Add repository layer

--- Lingua/Services/LinguaService.cs
+++ Lingua/Services/LinguaService.cs
@@ -3,43 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Lingua.Models;
+using Lingua.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace Lingua.Services
 {
 	public class LinguaService : ILinguaService
 	{
-		private readonly LinguaContext _context;
+		private readonly ICourseRepository _repository;
 
-		public LinguaService(LinguaContext context)
+		public LinguaService(ICourseRepository repository)
 		{
-			_context = context;
+			_repository = repository;
 		}
 
 		public async Task<IEnumerable<Course>> GetCourses()
 		{
-			return await _context.Courses.ToListAsync();
+			return await _repository.GetCourses();
 		}
 
 		public async Task<Course> CreateCourse(Course course)
 		{
-			_context.Courses.Add(course);
-			await _context.SaveChangesAsync();
-			return course;
+			return await _repository.CreateCourse(course);
 		}
 
 		public async Task<Course> UpdateCourse(Course course)
 		{
-			_context.Courses.Update(course);
-			await _context.SaveChangesAsync();
-			return course;
+			return await _repository.UpdateCourse(course);
 		}
 
 		public async Task DeleteCourse(int id)
 		{
-			var course = await _context.Courses.FirstOrDefaultAsync(c => c.Id == id);
-			_context.Courses.Remove(course);
-			await _context.SaveChangesAsync();
+			await _repository.DeleteCourse(id);
 		}
 	}
 }
